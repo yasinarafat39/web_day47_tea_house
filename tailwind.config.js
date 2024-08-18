267d1b476ef919c0896c6576f75d13dd345ce342
Yasin Arafat <yasinarafat2557@gmail.com>
2024-08-18 18:10:46 +0600

    header section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-1": "#363958",
+        "dark-2" : "#777777",
+        "hero-bg": "#FFFBF0",
+        "hero-orange": "#FABE4C",
+        "card-bg": "#F1F1F1",
+      },
+      spacing: {
+        "max-web-width": "1440px",
+        "max-content-width": "1170px",
+      },
+      fontFamily : {
+        "manrope" : ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
